d673d381d742f087c6e455d842413329331c2c80
grafox <g_fox_fts@yahoo.com>
2021-01-16 23:01:27 +0100

    firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCFyNliYi_ApJOINmbIa8G2NdAmHHtPqpo",
+    authDomain: "egav.firebaseapp.com",
+    databaseURL: "https://egav.firebaseio.com",
+    projectId: "firebase-egav",
+    storageBucket: "firebase-egav.appspot.com",
+    messagingSenderId: "140011368516"
+  }
 };
 
 /*
